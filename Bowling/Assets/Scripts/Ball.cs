8f53d99f42b04831006985974ea45326974fe8e0
Ethan <ethan.xiao@ible-tech.com>
2022-12-01 18:37:17 +0800

    1

--- Bowling/Assets/Scripts/Ball.cs
+++ Bowling/Assets/Scripts/Ball.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public GameObject ball;
+    Rigidbody rb;
+    [SerializeField]float force;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = ball.GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            rb.AddForce(Vector3.forward * force);
+        }
     }
 }
